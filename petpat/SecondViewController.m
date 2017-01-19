a07fd6fddfd4a21dfbd6ab38281389e0f0984049
perrakCWU <perra.kyle@gmail.com>
2017-01-19 09:01:04 -0800

    Initial Commit

---
+++ petpat/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  petpat
+//
+//  Created by Kyle on 1/19/17.
+//  Copyright © 2017 CWU. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ petpat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  petpat
+//
+//  Created by Kyle on 1/19/17.
+//  Copyright © 2017 CWU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
